cd31b22b2adc20bd05e3e1871fbf66f7b4728127
RohanBarshan <rohandeb2006@gmail.com>
2026-05-09 17:03:47 -0700

    Add: python-return-statement - submission-0

---
+++ Python For Beginners/python-return-statement/submission-0.py
@@ -0,0 +1,11 @@
+def product(m, n):
+    return m * n
+
+result = product(6,5)
+
+
+# don't modify below this line
+print(product(2, 4))
+print(product(8, 2))
+print(product(4, 8))
+print(product(8, 8))
