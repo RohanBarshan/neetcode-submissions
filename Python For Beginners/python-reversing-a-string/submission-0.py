c4f37d15990613896178cbcb4198efbfda81ff27
RohanBarshan <rohandeb2006@gmail.com>
2026-05-10 17:50:39 -0700

    Add: python-reversing-a-string - submission-0

---
+++ Python For Beginners/python-reversing-a-string/submission-0.py
@@ -0,0 +1,8 @@
+def reverse_string(input_string: str) -> str:
+    string_reverse = (input_string[::-1])
+    return string_reverse
+
+# do not modify below this line
+print(reverse_string("NeetCode"))
+print(reverse_string("Hello!"))
+print(reverse_string("Bye Bye"))
